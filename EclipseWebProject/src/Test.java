6a6e3575dd59f7f5ab31136ab13ce715c4579331
user600209 <bufigazu@macr2.com>
2020-05-15 15:06:06 +0900

    3回目

--- EclipseWebProject/src/Test.java
+++ EclipseWebProject/src/Test.java
@@ -27,7 +27,7 @@ public class Test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath()).append("second commit");
+		response.getWriter().append("Served at: ").append(request.getContextPath()).append("second commit").append("third commit");
 		
 	}
 
